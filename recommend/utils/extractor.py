4e32123464ea47db06fa359771b3cc3b41d83e18
amaneth <amankassahun190@gmail.com>
2021-11-23 14:06:03 +0000

    renamed contentId to content_id

--- recommend/utils/extractor.py
+++ recommend/utils/extractor.py
@@ -3,6 +3,6 @@ import pandas as pd
 class Extractor:
 
     def get_items_interacted(self, person_id, interactions_df):
-        interacted_items = interactions_df.loc[person_id]['contentId']
+        interacted_items = interactions_df.loc[person_id]['content_id']
         return set(interacted_items if type(interacted_items) is pd.Series else [interacted_items])
 
