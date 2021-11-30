52614367c57af27123a7aef90598a956f2802755
amaneth <amankassahun190@gmail.com>
2021-11-30 13:27:32 +0000

    added timestamp and source for interaction, changed content_id, person_id and author_person_id to char

--- recommend/models.py
+++ recommend/models.py
@@ -3,25 +3,25 @@ from jsonfield import JSONField
 #TODO content id as foreign key
 # Create your models here.
 class Interaction(models.Model):
-    person_id = models.IntegerField()
-    content_id = models.IntegerField()
+    person_id = models.CharField(max_length=60, default='')
+    content_id = models.CharField(max_length=60, default='')
     event_type = models.CharField(max_length=30, blank=True, default='')
+    timestamp = models.IntegerField(default=0)
+    source = models.CharField(max_length=40, default='')
 
     class Meta:
         unique_together= ('person_id','content_id','event_type',)
 
 class Article(models.Model):
     timestamp = models.IntegerField()
-    content_id = models.IntegerField()
-    author_person_id = models.IntegerField(default =0)
+    content_id = models.CharField(max_length=60, default='')
+    author_person_id = models.CharField(max_length=60, default='')
     author_country = models.CharField(max_length=60, blank=True, default='')
     url = models.CharField(max_length =400, blank=True, default ='')
     title = models.CharField(max_length =400, blank=True, default='')
     content = models.TextField()
+    source = models.CharField(max_length=40, default='mindplex')
 
 class Popularity(models.Model):
-    content_id = models.IntegerField(primary_key=True)
+    content_id = models.CharField(primary_key=True, max_length=60, default='')
     eventStrength = models.IntegerField()
-
-class UserProfile(models.Model):
-    profile = JSONField()
