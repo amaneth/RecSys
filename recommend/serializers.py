43d06a3fb79a7f25e07a18ae6acd9d7e68b1b636
amaneth <amankassahun190@gmail.com>
2021-11-30 13:29:00 +0000

    added timestamp and source

--- recommend/serializers.py
+++ recommend/serializers.py
@@ -1,11 +1,11 @@
 from rest_framework import serializers
-from recommend.models import Article, Interaction, UserProfile
+from recommend.models import Article, Interaction
 from django.db.models import Q
 
 class InteractionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Interaction
-        fields = ['person_id', 'content_id', 'event_type']
+        fields = ['person_id', 'content_id', 'event_type', 'timestamp','source']
     def create(self, validated_data):
         event = validated_data.get('event_type', None)
         if event=='unlike' or event == 'dislike':
@@ -14,7 +14,7 @@ class InteractionSerializer(serializers.ModelSerializer):
                     & Q(event_type='like'))
             q.delete()
 
-        if event=='unpositive' or event == 'react-negative':
+        if event=='unpositive':
             q = Interaction.objects.filter(Q(person_id=validated_data.get('person_id'))\
                     & Q(content_id=validated_data.get('content_id'))\
                     & Q(event_type='react-positive'))
@@ -43,17 +43,17 @@ class InteractionSerializer(serializers.ModelSerializer):
                 & Q(content_id=validated_data.get('content_id'))\
                 & Q(event_type='dislike'))
             q.delete()
-        
-        if event =='react-positive':
-            q = Interaction.objects.filter(Q(person_id=validated_data.get('person_id'))\
-                & Q(content_id=validated_data.get('content_id'))\
-                & Q(event_type='react-negative'))
-            q.delete()
 
         if event in ['like','react-positive','react-negative', 'dislike', 'comment-best', 
                         'comment-average', 'comment-good', 'view']:
+            interaction, created = Interaction.objects\
+                    .update_or_create(content_id= validated_data.get('content_id',None),
+                                        event_type=validated_data.get('event_type', None),
+                                        person_id=validated_data.get('person_id', None),
+                            defaults={'timestamp': validated_data.get('timestamp', None),
+                                        'source':  validated_data.get('source', None)})
             
-            return Interaction.objects.create(**validated_data)
+            return interaction
 
         else:
             return Interaction(**validated_data)
@@ -68,7 +68,7 @@ class InteractionSerializer(serializers.ModelSerializer):
 class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
-        fields = ['timestamp', 'content_id', 'author_person_id', 'author_country', 'url', 'title', 'content']
+        fields = ['timestamp', 'content_id', 'author_person_id', 'author_country', 'url', 'title', 'content','source']
     def create(self, validated_data):
         article, created = Article.objects.update_or_create(content_id= validated_data.get('content_id', None),
                             defaults={'timestamp': validated_data.get('timestamp', None),
@@ -76,11 +76,8 @@ class ArticleSerializer(serializers.ModelSerializer):
                                     'author_country':validated_data.get('author_country', None),
                                     'url': validated_data.get('url', None),
                                     'title': validated_data.get('title', None),
-                                    'content': validated_data.get('content', None)})
+                                    'content': validated_data.get('content', None),
+                                    'source': validated_data.get('source', None)})
         return article
 
 
-class UserProfileSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = UserProfile
-        fields = ['profile'] 
