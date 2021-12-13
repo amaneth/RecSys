adfb796a5e815e86a39202088b9558578dcc3221
amaneth <amankassahun190@gmail.com>
2021-12-13 08:50:54 +0000

    removed recommend from feature

--- recommend/utils/methods/contentive.py
+++ recommend/utils/methods/contentive.py
@@ -10,29 +10,28 @@ class ContentBasedRecommender:
     
     def __init__(self, items_df=None):
         self.items_df = items_df
-        with open('cbmodel.pickle', 'rb') as handle:
-            self.model=pickle.load(handle)
-        '''with open('userprofile.pickle', 'rb') as handle:
+        with open('itemids.pickle', 'rb') as handle:
+            self.item_ids=pickle.load(handle)
+        with open('userprofile.pickle', 'rb') as handle:
             self.user_profiles = pickle.load(handle)
-        self.tfidf_matrix = sparse.load_npz("tfidf.npz")'''
+        self.tfidf_matrix = sparse.load_npz("tfidf.npz")
         
     def get_model_name(self):
         return self.MODEL_NAME
         
-    def _get_similar_items_to_user_profile(self, person_id,source='mindplex',topn=1000):
+    def _get_similar_items_to_user_profile(self, person_id,topn=1000):
         #Computes the cosine similarity between the user profile and all item profiles
-        cosine_similarities = cosine_similarity(self.user_profiles[person_id], 
-                                    self.model['tfidf'][source])
+        cosine_similarities = cosine_similarity(self.user_profiles[person_id], self.tfidf_matrix)
         #Gets the top similar items
         similar_indices = cosine_similarities.argsort().flatten()[-topn:]
         #Sort the similar items by similarity
-        similar_items = sorted([(self.model['ids'][source], cosine_similarities[0,i])\
+        similar_items = sorted([(self.item_ids[i], cosine_similarities[0,i])\
                                 for i in similar_indices],
                 key=lambda x: -x[1])
         return similar_items
         
-    def recommend_items(self,recommend_from='mindplex', user_id, items_to_ignore=[], topn=10, verbose=False):
-        similar_items = self._get_similar_items_to_user_profile(user_id, source=recommend_from)
+    def recommend_items(self, user_id, items_to_ignore=[], topn=10, verbose=False):
+        similar_items = self._get_similar_items_to_user_profile(user_id)
         #Ignores items the user has already interacted
         similar_items_filtered = list(filter(lambda x: x[0] not in items_to_ignore, similar_items))
         
