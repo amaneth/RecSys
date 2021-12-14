de5d30cec2589a455e6f5bcc57b21d70b435eb1d
aman <amankassahun190@gmail.com>
2021-12-14 16:36:51 +0300

    separate recommendation for different sources

--- recommend/utils/models.py
+++ recommend/utils/models.py
@@ -7,6 +7,7 @@ from sqlalchemy import create_engine
 from celery.utils.log import get_task_logger
 from django.core.cache import cache
 from django_pandas.io import read_frame
+from django.db.models import Q
 
 import pickle
 import numpy as np
@@ -40,10 +41,14 @@ logger.addHandler(loghandle)
 
 
 class MlModels:
-    def __init__(self):
+    def __init__(self, model):
         logger.info("Initializing building models...")
-        self.interactions_df = read_frame(Interaction.objects.filter(source='mindplex'))
-        self.interactions_df = self.interactions_df[(self.interactions_df['event_type']!='dislike')\
+        self.content_based_model_data={}
+        self.interactions_df = read_frame(Interaction.objects.all())
+        logger.debug("Shape of interactions df : -{0}- ".format(str(self.interactions_df.shape)))
+        if model=='content-based':
+            self.interactions_df = \
+                    self.interactions_df[(self.interactions_df['event_type']!='dislike')\
                 & (self.interactions_df['event_type']!='react-negative')]
         self.event_type_strength= {'view': 0.5,
                 'like':1.0,
@@ -55,25 +60,28 @@ class MlModels:
                 'comment-good':2.0}
         self.interactions_df['eventStrength'] = self.interactions_df['event_type']\
                 .apply(lambda x: self.event_type_strength[x])
-        self.interactions_df = self.interactions_df.groupby(['content_id', 'person_id'])\
+        self.interactions_df = self.interactions_df.groupby(['content_id', 'person_id', 'source'])\
                             ['eventStrength'].sum().apply(lambda x : math.log(1+x, 2) if x>=0\
                             else -math.log(1+abs(x), 2) ).reset_index()
+        logger.debug("Shape of interactions df after calculating event strenght: -{0}- "\
+                .format(str(self.interactions_df.shape)))
         #self.interactions_df = pd.read_csv('recommend/files/interactions.csv')
         self.articles_df = read_frame(Article.objects.all())
         self.mindplex_articles_df= read_frame(Article.objects.filter(source='mindplex'))
-        self.crawled_articles_df= read_frame(Article.objects.filter(source!='mindplex'))
-        self.articles_df = pd.read_csv('recommend/files/articles.csv')
-        interaction_train_df = train_test_split(self.interactions_df)                    
-        self.interactions_train_df = pd.read_csv('recommend/files/interactions_train.csv')
+        self.crawled_articles_df= read_frame(Article.objects.filter(~Q(source='mindplex')))
+        #self.articles_df = pd.read_csv('recommend/files/articles.csv')
+        #interaction_train_df = train_test_split(self.interactions_df)                    
+        #self.interactions_train_df = pd.read_csv('recommend/files/interactions_train.csv')
         self.interactions_train_df = self.interactions_df
         self.item_ids= self.articles_df['content_id'].tolist()
         self.tfidf_matrix = None
 
     def popularity(self):
+        logger.info("building the popularity model has started")
         self.interactions_df.set_index('person_id', inplace=True)
-        popularity_df = self.interactions_df.groupby('content_id')['eventStrength'].sum(). \
-                            sort_values(ascending= False).reset_index()
-
+        popularity_df = self.interactions_df.groupby(['content_id', 'source'])['eventStrength']\
+                .sum().sort_values(ascending= False).reset_index()
+        logger.debug("Shape of articles in the popularity: -{0}- ".format(str(popularity_df.shape)))
         user = settings.DATABASES['default']['USER']
         password = settings.DATABASES['default']['PASSWORD']
         database_name = settings.DATABASES['default']['NAME']
@@ -86,11 +94,11 @@ class MlModels:
 
         popularity_df.to_sql(Popularity._meta.db_table, con=engine, if_exists='replace') # saving to the database
         unlock = cache.delete("popularity")
-        logger.debug("Release lock is  done is :"+ str(unlock))
+        logger.info("Release lock is  done is :"+ str(unlock))
     
     def tfidf(self):
         logger.info("Building tfidf model..")
-        stopwords_list = stopwords.words('english') + stopwords.words('portuguese')
+        stopwords_list = stopwords.words('english')
         vectorizer = TfidfVectorizer(analyzer='word',
                             min_df=0.003,
                             max_df=0.5,
@@ -109,12 +117,11 @@ class MlModels:
                                 crawled_articles_df['title'])
         tfidf_by_source= {'mindplex': mindplex_tfidf_matrix,
                 'crawled': crawled_tfidf_matrix}
-        content_based_model_data= {'tfidf':tfidf_by_source,
+        self.content_based_model_data= {'tfidf':tfidf_by_source,
                                     'ids': article_ids,
                                     'feature_names': tfidf_feature_names
                                  }
-        with open('cbmodel.pickle', 'wb') as handle:
-            pickle.dump(content_based_model_data, handle, protocol= pickle.HIGHEST_PROTOCOL)
+        #TODO incremental concept should be implemented here
         '''with open('itemids.pickle', 'wb') as handle:
             pickle.dump(self.item_ids, handle, protocol = pickle.HIGHEST_PROTOCOL)
         sparse.save_npz("tfidf.npz", self.tfidf_matrix)'''
@@ -151,8 +158,14 @@ class MlModels:
         user_profiles = {}
         for person_id in interactions_indexed_df.index.unique():
             user_profiles[person_id] = self.build_users_profile(person_id, interactions_indexed_df)
-        with open('userprofile.pickle', 'wb') as handle:
+        '''with open('userprofile.pickle', 'wb') as handle:
             pickle.dump(user_profiles, handle, protocol=pickle.HIGHEST_PROTOCOL)
-            logger.info("Saving the users profile to the database profile is done"+ str(os.getcwd()))
+            logger.info("Saving the users profile to the database profile is done"+\
+                    str(os.getcwd()))'''
+        self.content_based_model_data['user_profiles']= user_profiles
+        with open('cbmodel.pickle', 'wb') as handle:
+            pickle.dump(self.content_based_model_data, handle, protocol= pickle.HIGHEST_PROTOCOL)
+            logger.info("Saving the users profile to the database profile is done"+\
+                    str(os.getcwd()))
         unlock = cache.delete("content-based")
         logger.debug("Release lock for content-based is  done is :"+ str(unlock))
