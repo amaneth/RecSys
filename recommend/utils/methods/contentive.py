bd3fdb1cfb7a59302f709fb8c6a8c700bd619f4c
aman <amankassahun190@gmail.com>
2021-12-14 16:37:57 +0300

    separate recommendation for different sources or communities

--- recommend/utils/methods/contentive.py
+++ recommend/utils/methods/contentive.py
@@ -3,6 +3,21 @@ import pickle
 from scipy import sparse
 from sklearn.metrics.pairwise import cosine_similarity
 import pandas as pd
+import os
+import logging
+import logging.handlers as loghandlers
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+loghandle = loghandlers.TimedRotatingFileHandler(
+                        filename="RecSys.log",
+                                        when='D', interval=1, backupCount=7,
+                                                        encoding="utf-8")
+loghandle.setFormatter(
+            logging.Formatter("%(asctime)s %(message)s"))
+logger.addHandler(loghandle)
+
+
 
 class ContentBasedRecommender:
     
@@ -21,18 +36,19 @@ class ContentBasedRecommender:
         
     def _get_similar_items_to_user_profile(self, person_id,source='mindplex',topn=1000):
         #Computes the cosine similarity between the user profile and all item profiles
-        cosine_similarities = cosine_similarity(self.user_profiles[person_id], 
+        cosine_similarities = cosine_similarity(self.model['user_profiles'][person_id], 
                                     self.model['tfidf'][source])
         #Gets the top similar items
         similar_indices = cosine_similarities.argsort().flatten()[-topn:]
         #Sort the similar items by similarity
-        similar_items = sorted([(self.model['ids'][source], cosine_similarities[0,i])\
+        similar_items = sorted([(self.model['ids'][source][i], cosine_similarities[0,i])\
                                 for i in similar_indices],
                 key=lambda x: -x[1])
         return similar_items
         
-    def recommend_items(self,recommend_from='mindplex', user_id, items_to_ignore=[], topn=10, verbose=False):
+    def recommend_items(self,user_id, recommend_from='mindplex', items_to_ignore=[], topn=10, verbose=False):
         similar_items = self._get_similar_items_to_user_profile(user_id, source=recommend_from)
+        logger.info("{0} similar articles has been found:".format(str(similar_items)))
         #Ignores items the user has already interacted
         similar_items_filtered = list(filter(lambda x: x[0] not in items_to_ignore, similar_items))
         
