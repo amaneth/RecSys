1954ef80deb6350f11ea06ca65be2a537a4fc117
aman <amankassahun190@gmail.com>
2021-12-17 10:04:30 +0300

    handled for users with no previous interaction

--- recommend/utils/extractor.py
+++ recommend/utils/extractor.py
@@ -3,6 +3,10 @@ import pandas as pd
 class Extractor:
 
     def get_items_interacted(self, person_id, interactions_df):
-        interacted_items = interactions_df.loc[person_id]['content_id']
-        return set(interacted_items if type(interacted_items) is pd.Series else [interacted_items])
+        try:
+            interacted_items = interactions_df.loc[person_id]['content_id']
+            return set(interacted_items if type(interacted_items) \
+                    is pd.Series else [interacted_items])
+        except KeyError:
+            return set()
 
