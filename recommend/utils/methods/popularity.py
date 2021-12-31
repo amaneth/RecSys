d7481b76b2530977cc763bed220c845c1dd20fde
aman <amankassahun190@gmail.com>
2021-12-31 15:19:00 +0300

    removed the verbose mode

--- recommend/utils/methods/popularity.py
+++ recommend/utils/methods/popularity.py
@@ -1,36 +1,22 @@
+from django_pandas.io import read_frame
+from recommend.models import Popularity
+from django.db.models import Q
+
 class PopularityRecommender:
     MODEL_NAME = 'Popularity'
     
-    def __init__(self, popularity_df, items_df=None):
-        self.popularity_df = popularity_df
-        self.items_df = items_df
+    def __init__(self, recommend_from):
+        if recommend_from=='mindplex':
+            self.popularity_df = read_frame(Popularity.objects.filter(source='mindplex'))
+        else:
+            self.popularity_df = read_frame(Popularity.objects.filter(~Q(source='mindplex')))
         
     def get_model_name(self):
         return self.MODEL_NAME
-    def recommend_items(self, user_id, recommend_from='mindplex', items_to_ignore=[], topn=10, verbose=False):
+    def recommend_articles(self, user_id, articles_to_ignore):
         # Recommend the more popular itmes that the user hasn't seen yet.
         recommendations_df = self.popularity_df[~self.popularity_df['content_id']\
-                                .isin(items_to_ignore)] \
-                                .sort_values('eventStrength', ascending=False) \
-                                .head(topn)
+                                .isin(articles_to_ignore)] \
+                                .sort_values('eventStrength', ascending=False) 
         
-        #recommendations_popular_df = self.popularity_df[~self.popularity_df['content_id']\
-        #                        .isin(items_to_ignore)] \
-        #                        .sort_values('eventStrength', ascending=False)
-        #if source =='mindplex':
-        #    recommendations_source_filtered_df = recommendations_popular_filtered_df.\
-        #            loc[recommendations_popular_df['source']==source]
-        #else:
-        #    recommendations_source_filtered_df = recommendations_popular_filtered_df.\
-        #            loc[recommendations_popular_df['source']!='mindplex']
-        #recommendations_df= recommendations_source_filtered_df[['content_id','recStrength']]\
-        #        .head(topn)
-        if verbose:
-            if self.items_df is None:
-                raise Exception('"items_df" is required in verbose mode')
-            recommendations_df = recommendations_df.merge(self.items_df, how = 'left', 
-                                                          left_on = 'content_id', 
-                                                          right_on = 'content_id')\
-                                                          [['eventStrength', 'content_id', 
-                                                              'title', 'url']]
         return recommendations_df
