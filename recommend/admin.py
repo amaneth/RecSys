daac70ea74c6c937d8119dc3ca688da9d069d345
aman <amankassahun190@gmail.com>
2022-01-03 14:45:40 +0300

    register model to admin

--- recommend/admin.py
+++ recommend/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from recommend.models import *
 # Register your models here.
+myModels = [Reputation, Article, Interaction, Popularity]
+admin.site.register(myModels)
