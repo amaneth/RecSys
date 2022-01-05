41a7199dcde0a379d2132c95719bdd2311f55a55
aman <amankassahun190@gmail.com>
2022-01-05 18:37:46 +0300

    added community id

--- recommend/utils/methods/contentive.py
+++ recommend/utils/methods/contentive.py
@@ -24,8 +24,9 @@ class ContentBasedRecommender:
     
     MODEL_NAME = 'Content-Based'
     
-    def __init__(self, recommend_from):
+    def __init__(self, recommend_from, community):
         self.source= recommend_from
+        self.community = community
         with open('cbmodel.pickle', 'rb') as handle:
             self.model=pickle.load(handle)
         
@@ -35,25 +36,26 @@ class ContentBasedRecommender:
     def _get_similar_items_to_user_profile(self, person_id, topn=1000):
         #Computes the cosine similarity between the user profile and all item profiles
         try:
-            cosine_similarities = cosine_similarity(self.model['user_profiles'][person_id], 
-                                    self.model['tfidf'][self.source])
-            logger.debug("The shape of the user pirofiles is: {}".format(str(self.model['user_profiles'][person_id].shape)))
+            cosine_similarities = cosine_similarity(self.model[self.community]['user_profiles'][person_id], 
+                                    self.model[self.community]['tfidf'][self.source])
+            logger.debug("The shape of the user pirofiles is: {}"\
+                    .format(str(self.model[self.community]['user_profiles'][person_id].shape)))
         except KeyError:
-            dummy_profile = np.array([np.zeros(len(list(self.model['user_profiles']\
+            dummy_profile = np.array([np.zeros(len(list(self.model[self.community]['user_profiles']\
                     .values())[0][0]))])
-            cosine_similarities = cosine_similarity(dummy_profile, self.model['tfidf'][self.source])
+            cosine_similarities = cosine_similarity(dummy_profile, self.model[self.community]['tfidf'][self.source])
             logger.info("User is getting some random content-based recommendation\
                     because it has no previous history")
         #Gets the top similar items
         similar_indices = cosine_similarities.argsort().flatten()[-topn:]
         #Sort the similar items by similarity
-        similar_items = sorted([(self.model['ids'][self.source][i], cosine_similarities[0,i])\
+        similar_items = sorted([(self.model[self.community]['ids'][self.source][i], cosine_similarities[0,i])\
                                 for i in similar_indices],
                 key=lambda x: -x[1])
         return similar_items
         
     def recommend_articles(self,user_id, articles_to_ignore):
-        if self.model['tfidf'][self.source] is None:
+        if self.model[self.community]['tfidf'][self.source] is None:
             return pd.DataFrame()
         similar_items = self._get_similar_items_to_user_profile(user_id)
         logger.info("{0} similar articles has been found:".format(str(similar_items)))
