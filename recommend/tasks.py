03deaab61b814ca575988515f961b7c06fa39a7d
aman <amankassahun190@gmail.com>
2022-01-05 18:41:25 +0300

    added community

--- recommend/tasks.py
+++ recommend/tasks.py
@@ -13,29 +13,29 @@ LOCK_EXPIRE = 60
 
 
 @shared_task
-def popularity_relearn():
+def popularity_relearn(community):
     acquired = cache.add("popularity","popularity",LOCK_EXPIRE)
     logger.debug("Popularity C----A---C---H--ED is : %s", acquired)
     if acquired:
         logger.info('Popularity model is relearning...')
-        model=MlModels('popularity')
+        model=MlModels('popularity', community)
         model.popularity()
     else:
         logger.debug('Relearning the popularity model is already started by another worker')
 
 @shared_task
-def content_based_relearn():
+def content_based_relearn(community):
     acquired = cache.add("content-based","content-based",LOCK_EXPIRE)
     logger.debug("Content based C----A---C---H--ED is : %s", acquired)
     if acquired:
         logger.info('Content based model is relearning...')
-        model=MlModels('content-based')
+        model=MlModels('content-based', community)
         model.build_users_profiles()
     else:
         logger.debug('Relearning the content based model is already started by another worker')
 
 @shared_task
-def collaborative_relearn():
+def collaborative_relearn(community):
     acquired = cache.add("collaborative","collaborative",LOCK_EXPIRE)
     logger.debug("Collaborative C----A---C---H--ED is : %s", acquired)
     if acquired:
@@ -47,12 +47,12 @@ def collaborative_relearn():
 
 
 @shared_task
-def high_quality_relearn():
+def high_quality_relearn(community):
     acquired = cache.add("high-quality","high-quality",LOCK_EXPIRE)
     logger.debug("High quality model relearn started cached is : %s", acquired)
     if acquired:
         logger.info('High Quality model is relearning...')
-        model=MlModels('high-quality')
+        model=MlModels('high-quality', community)
         model.build_users_reputation()
     else:
         logger.debug('Relearning the high quality model is already started by another worker')
